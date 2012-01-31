3570170181915ff89105c63651d7a43e034e9ed2
Daniel Braginsky <danbraginsky@gmail.com>
2012-01-31 13:42:03 -0800

    Evaluates Roman Numerals, but only if one subtraction rule

--- Euler89.rb
+++ Euler89.rb
@@ -6,43 +6,40 @@ class Euler89
 	def initialize()
 		@arrOfRomans = []
 		@file = File.new("roman.txt", "r")  #Brings in the file\
-		@arrOfRoman = {"I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100, "C" => 100, "D" => 500, "M" => 1000}
-		@arrOfValue = {1=>"I", 5=>"V", 10=>"X", 50=>"L", 100=>"C", 500=>"D", 1000=>"M"}
+		@hashOfRoman = {"I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100, "D" => 500, "M" => 1000}
+		@hashOfValue = {1=>"I", 5=>"V", 10=>"X", 50=>"L", 100=>"C", 500=>"D",1000=>"M"}
+		@hashOfSRoman = {"IV"=>4, "IX" => 9, "XL" => 40, "XC" =>90, "CD"=>400, "CM"=>900}
+		@hashOfSValue = {4=>"IV", 9=>"IX", 40=>"XL", 90=>'XC', 400=>'CD', 900=>'CM'}
 		while (line = @file.gets)  #Cycles through all the lines
 			@arrOfRomans.push(line)  #Adds each line into arrOfRomans
 		end  #endWhile
 		@ArrOfIdeals = []
 	end  #endDef
-	
+
 	def romanIntoNum(arg)
-		i = 0
 		value = 0
-		lastNum = arg[0]
-		while i < arg.length
-			if (@arrOfRDigits.index(arg[i]) >= @arrOfRDigits.index(lastNum))
-				print 'Yay', "\n"
+		@hashOfSRoman.each do|roman,arabic|
+			if(arg.include?(roman))
+				value+=arabic
+				arg = arg.delete(roman)
 			end
-			value+=@arrOfValues[@arrOfRDigits.index(arg[i])]
-			i+=1
 		end
-		return value
+		
+		@hashOfRoman.each do|roman,arabic|
+			if(arg.include?(roman))
+				value+=arabic
+				arg = arg.delete(roman)
+			end
+		end
+		print value, "\n"
 	end
 	
-	def idealRoman(int)
-		roman = ''
-		while int > 0
-			for ss in @arrOfRoman
-				if (ss > int)
-					print 'happy'
-					#print @arrOfValue.keys[ss - 1]
-				end
-			end
-		end
-		return roman
+	def numIntoRoman(num)
+		
 	end
 end
 
 whatever = Euler89.new()
-# print whatever.romanIntoNum("IV"), "\n"
+whatever.romanIntoNum("MIV")
 # print whatever.romanIntoNum("VI"), "\n"
-print whatever.idealRoman(6)
+# print whatever.idealRoman(6)
