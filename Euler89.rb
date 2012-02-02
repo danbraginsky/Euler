cdc2cf79cd84f3c5ee8b2474ca26feaa9c96f98d
Daniel Braginsky <danbraginsky@gmail.com>
2012-02-02 10:23:48 -0800

    Finished arabic=>roman, works with subtraction rule. Still need to find final answer

--- Euler89.rb
+++ Euler89.rb
@@ -7,9 +7,10 @@ class Euler89
 		@arrOfRomans = []
 		@file = File.new("roman.txt", "r")  #Brings in the file\
 		@hashOfRoman = {"I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100, "D" => 500, "M" => 1000}
-		@hashOfValue = {1=>"I", 5=>"V", 10=>"X", 50=>"L", 100=>"C", 500=>"D",1000=>"M"}
+		#@hashOfValue = {1=>"I", 5=>"V", 10=>"X", 50=>"L", 100=>"C", 500=>"D",1000=>"M"}
 		@hashOfSubRoman = {"IV"=>4, "IX" => 9, "XL" => 40, "XC" =>90, "CD"=>400, "CM"=>900}
-		@hashOfSubValue = {4=>"IV", 9=>"IX", 40=>"XL", 90=>'XC', 400=>'CD', 900=>'CM'}
+		#@hashOfSubValue = {4=>"IV", 9=>"IX", 40=>"XL", 90=>'XC', 400=>'CD', 900=>'CM'}
+		@hashOfTotalRoman = {"M"=>1000,"CM"=>900,"D"=>500,"CD"=>400,"C"=>100,"XC"=>90,"L"=>50,"XL"=>40,"X"=>10,"IX"=>9,"V"=>5,"IV"=>4,"I"=>1}
 		while (line = @file.gets)  #Cycles through all the lines
 			@arrOfRomans.push(line)  #Adds each line into arrOfRomans
 		end  #endWhile
@@ -34,11 +35,16 @@ class Euler89
 	end
 	
 	def numIntoRoman(num)
-		
+		returnRoman = ""
+		@hashOfTotalRoman.each do|roman,arabic|
+			while(arabic<=num)
+				returnRoman = returnRoman + roman
+				num-=arabic
+			end
+		end
+		print returnRoman
 	end
 end
 
 whatever = Euler89.new()
-whatever.romanIntoNum("MMIV")
-# print whatever.romanIntoNum("VI"), "\n"
-# print whatever.idealRoman(6)
+whatever.numIntoRoman(41)
